6d29191f462810d5a7f628ad3aaf1605a5e6949f
yandaki <boayves@gmail.com>
2023-06-25 17:33:19 +0000

    setting up the layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,49 @@ import { useSelector } from 'react-redux/es/hooks/useSelector'
 import { useDispatch } from 'react-redux'
 import { apiQuote } from './api/quote-api'
 import quoteSlice, { quotesActions } from './store/quoteSlice'
+import styled from 'styled-components'
+import '/src/App.css'
 
 
 function App() {
+
+  const Title = styled.h1`
+    color:'white';
+    font-size:55px;
+    text-align:center;
+  `;
+
+  const TextQuote = styled.p`
+    font-size:25px;
+  `
+
+  const BottomPart = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-around;
+
+    padding-top:40px;
+  `
+
+  const ButtonGen = styled.button`
+    color:white;
+    font-size:20px;
+    width:140px;
+    height:40px;
+    border:none;
+    background-color:black;
+    cursor:pointer;
+  `
+
+  const WrapperIconShare = styled.div`
+    display:flex;
+    gap:2em;
+  `
+
+  const WrapperQuote = styled.div`
+    padding: 2.5em;
+  `
+
   const [count, setCount] = useState(0)
 
   // use hook useDispatch for execute 
@@ -14,11 +54,16 @@ function App() {
   const quotes = useSelector(state => state.quotes)
   console.log('Quotes',quotes)
 
+  // Get array of Quotes
   const getQuotes = async () =>{
     await apiQuote.get()
       .then((data) =>{
         dispatch(quotesActions.setQuotes(data.data))
-    })
+      }).catch(err => console.log(err))
+  }
+
+  const testGen = () =>{
+    dispatch(quotesActions.generateQuote())
   }
 
   useEffect(() =>{
@@ -31,21 +76,31 @@ function App() {
   
   return (
     <>
-      <div>
-       
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
+      <Title>Random Quotes</Title>
+      <WrapperQuote id="main">
+        <TextQuote id="test-quote">
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </TextQuote>
+        
+        {/* Part Bottom */}
+        <BottomPart id="part-bottom">
+          {/* Icon Share Block */}
+          <WrapperIconShare id='icon-share'>
+            <ButtonGen onClick={testGen}>
+              Twitter
+            </ButtonGen>
+            <ButtonGen onClick={testGen}>
+              Facebook
+            </ButtonGen>
+          </WrapperIconShare>
+          
+          {/* Button New Quote */}
+          <ButtonGen onClick={testGen}>
+            New Quote
+          </ButtonGen>
+        </BottomPart>
+      </WrapperQuote>
     </>
   )
 }
--- src/store/quoteSlice.js
+++ src/store/quoteSlice.js
@@ -4,11 +4,39 @@ import { createSlice } from "@reduxjs/toolkit";
 const quoteSlice = createSlice({
     name:'quotes',
     initialState:{
-        quotes:[]
+        quotes:[],
+        quote:'',
+        quoteAuthor:''
     },
     reducers:{
         setQuotes(state , action){
             state.quotes = action.payload
+
+            // Random number 
+            const rn = Math.floor(Math.random() * state.quotes.length)
+
+            // Quote 
+            state.quote = state.quotes[rn].text
+            // Author of quote
+            state.quoteAuthor = state.quotes[rn].author 
+        },
+        generateQuote(state , action){
+            // Generate a number between 0 and the length of quotes length.
+            const lengthQuotes = state.quotes.length
+            // console.log('Length :', lengthQuotes)
+
+            // Random number
+            const randomNumber = Math.floor(Math.random() * lengthQuotes)
+            console.log("Random number generate", randomNumber)
+
+            // quote
+            state.quote = state.quotes[randomNumber].text
+            // quote author
+            state.quoteAuthor = state.quotes[randomNumber].author
+
+            // console.log quote 
+            // console.log(state.quote)
+            // console.log(state.quoteAuthor)
         }
     }
 })
